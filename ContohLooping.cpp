baac5854028104115635fac49a44294e8aabeaa7
Daffansyah276 <daffansyaharya@gmail.com>
2025-03-17 16:25:50 +0700

    menambahkan increment

--- ContohLooping.cpp
+++ ContohLooping.cpp
@@ -11,3 +11,16 @@ int main ()
     int perulanganWhile = 0;
     int perulanganDo = 0;
 
+     // Perulanagn Dengan for
+
+     cout << "Perulangan for pencacah naik" << endl;
+     for (n = 0; n < 5; n++)
+     {
+     cout << "Nilai n = " << n << "Selamat Datang" << endl;
+     }
+     cout << "Nilai n terakhir = " << n << endl;
+     cout << endl;
+ 
+ 
+ 
+
